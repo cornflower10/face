c2909be7eaeaee4d200891cf6591ca17d6b7707d
xiejingbao <xiejb@qlzqzg.com>
2019-05-10 18:31:38 +0800

    Initial commit

---
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "5gryBjYxWogiCvptoPio6BKvkdq737smdQpHmLgcNYKU";
+    public static final String SDK_KEY = "7HsevbBamWXbqxXm68oFKYTMme9s9bAaXW9aUZbsNwCL";
+}
